1569d2f1daddde057af2e3f1c42677151ca6c80e
jayaprakashvfx11322-rgb <jayaprakashvfx11322@gmail.com>
2026-06-23 21:58:00 +0530

    Add mock avatars and new image URLs

--- utils/mockData.ts
+++ utils/mockData.ts
@@ -1,6 +1,17 @@
+export const MOCK_AVATARS = [
+  "https://i.pravatar.cc/300?img=1",
+  "https://i.pravatar.cc/300?img=2",
+  "https://i.pravatar.cc/300?img=3",
+  "https://i.pravatar.cc/300?img=4",
+  "https://i.pravatar.cc/300?img=5",
+  "https://i.pravatar.cc/300?img=6"
+];
+
 export const MOCK_IMAGES = {
   sunsetOcean: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
-  setup: "https://images.unsplash.com/photo-1516321318423-f06f85e504b3"
+  setup: "https://images.unsplash.com/photo-1516321318423-f06f85e504b3",
+  defaultBanner: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+  defaultProfile: "https://i.pravatar.cc/300"
 };
 
 export const INITIAL_MOCK_USERS = [];
