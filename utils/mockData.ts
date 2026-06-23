fcbef8930a9c6d74e1dee7f81e04421225e4b0ec
jayaprakashvfx11322-rgb <jayaprakashvfx11322@gmail.com>
2026-06-23 21:37:40 +0530

    Update mockData.ts

--- utils/mockData.ts
+++ utils/mockData.ts
@@ -1,3 +1,9 @@
 export const MOCK_IMAGES = {
   sunsetOcean: "https://images.unsplash.com/photo-1506744038136-46273834b3fb"
 };
+
+export const INITIAL_MOCK_USERS = [];
+
+export const INITIAL_MOCK_POSTS = [];
+
+export const INITIAL_MOCK_REELS = [];
